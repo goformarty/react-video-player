1e68aac6743b37d92cbc1f063d3900cb4a443fe9
Marty Stepien <marty.stepien@gmail.com>
2018-05-19 22:15:52 +0100

    cleaning up class search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
 
-// const SearchBar = () => {
-// 	return <input />
-// };
-
 class SearchBar extends Component {
-	constractor(props) {
+	constructor(props) {
 		super(props);
-
 		this.state = { term: '' }
 	}
 
 	render() {
 		return (
-		<input onChange={event => this.setState({term: event.target.value})} />
-		// return <input onChange = {this.onInputChange} />
+			<div>
+				<input onChange={event => this.setState({term: event.target.value})} />
+				Value of the input: {this.state.term}
+			</div>
 		)
 	}
 
-	// onInputChange(event) {
-	// 	console.log(event.target.value);
-	// }
 }
 
 export default SearchBar;
